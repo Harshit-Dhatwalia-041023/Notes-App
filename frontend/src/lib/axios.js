3433541bc9e27360a62dfd99e94cbad3f325c89a
Harshit-Dhatwalia-041023 <dhatwaliaharshit01@gmail.com>
2025-11-12 05:28:05 +0530

    Route update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const BASE_URL = import.meta.env.MODE==="development" ? "http://localhost:3000/api" : "api"
+// const BASE_URL = import.meta.env.MODE==="development" ? "http://localhost:3000/api" : "/api"
 const api = axios.create({
-    baseURL:BASE_URL,
+    baseURL: "http://localhost:3000/api"
+    // BASE_URL,
 })
 export default api;
