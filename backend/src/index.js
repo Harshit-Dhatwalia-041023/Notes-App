52e18b819a5c0d5feec8e195438b5f2f0acad3fb
Harshit-Dhatwalia-041023 <dhatwaliaharshit01@gmail.com>
2025-11-12 04:40:44 +0530

    Deployment changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,37 @@
-if(process.env.NODE_ENV != "production"){
+if(process.env.NODE_ENV !== "production"){
     require("dotenv").config()
 }
 const express = require("express");
 const app = express();
-const port = process.env.PORT||3000
+const path = require("path");
+const cors = require("cors");
+const port = process.env.PORT||3000;
+
 const router = require("./routes/notesRoutes");
 const dbconnect = require("./config/db");
 const rateLimiter = require("./middleware/rateLimiter");
-const cors = require("cors");
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
-app.use((cors()));
+if(process.env.NODE_ENV !== "production"){
+    app.use((cors()));
+}
+
 app.use(rateLimiter);
 
 app.use("/api/notes",router);
+
+if (process.env.NODE_ENV === "production") {
+    const fronendPath = path.join(__dirname,"../../frontend/dist");
+    console.log(fronendPath);
+  app.use(express.static(fronendPath));
+
+  app.use( (req, res) => {
+  res.sendFile(path.join(fronendPath,"index.html"));
+});
+}
 
 app.use((err,req,res,next)=>{
     res.status(400).json({mssg:"Error during request"})
